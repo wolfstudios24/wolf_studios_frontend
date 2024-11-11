d6ed0fc3d927365b00188fe9d3be1ae7862e7a05
fazlyedgenroots <fazly@edgenroots.com>
2024-11-11 20:13:52 +0600

    first commit

---
+++ src/app/layout.js
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import InitColorSchemeScript from '@mui/material/InitColorSchemeScript';
+
+import '@/styles/global.css';
+
+import { config } from '@/config';
+import { applyDefaultSettings } from '@/lib/settings/apply-default-settings';
+import { getSettings as getPersistedSettings } from '@/lib/settings/get-settings';
+import { UserProvider } from '@/contexts/auth/user-context';
+import { SettingsProvider } from '@/contexts/settings';
+import { Analytics } from '@/components/core/analytics';
+import { I18nProvider } from '@/components/core/i18n-provider';
+import { LocalizationProvider } from '@/components/core/localization-provider';
+import { SettingsButton } from '@/components/core/settings/settings-button';
+import { ThemeProvider } from '@/components/core/theme-provider/theme-provider';
+import { Toaster } from '@/components/core/toaster';
+
+export const metadata = { title: config.site.name };
+
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  themeColor: config.site.themeColor,
+};
+
+export default async function Layout({ children }) {
+  const settings = applyDefaultSettings(await getPersistedSettings());
+
+  return (
+    <html lang={settings.language} suppressHydrationWarning>
+      <body>
+        <InitColorSchemeScript attribute="class" />
+        <Analytics>
+          <LocalizationProvider>
+            <UserProvider>
+              <SettingsProvider settings={settings}>
+                <I18nProvider lng={settings.language}>
+                  <ThemeProvider>
+                    {children}
+                    <SettingsButton />
+                    <Toaster position="bottom-right" />
+                  </ThemeProvider>
+                </I18nProvider>
+              </SettingsProvider>
+            </UserProvider>
+          </LocalizationProvider>
+        </Analytics>
+      </body>
+    </html>
+  );
+}
