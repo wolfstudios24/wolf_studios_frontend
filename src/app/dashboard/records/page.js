b1d35a0389ad73ecabdbd68f40e94eb3266e8c81
codemine24 <codemine24@gmail.com>
2024-12-14 07:32:24 +0600

    editable data grid added in record table.

--- src/app/dashboard/records/page.js
+++ src/app/dashboard/records/page.js
@@ -2,8 +2,6 @@
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
-import Stack from '@mui/material/Stack';
-import RouterLink from 'next/link';
 
 import Typography from '@mui/material/Typography';
 import { Plus as PlusIcon } from '@phosphor-icons/react/dist/ssr/Plus';
@@ -12,25 +10,36 @@ import * as React from 'react';
 import PageLoader from '@/components/PageLoader/PageLoader';
 import IconButton from '@mui/material/IconButton';
 
-import { DataTable } from '@/components/core/data-table';
-import { FilterButton } from '@/components/core/filter-button';
-import { StatusFilterPopover } from '@/components/core/filters/StatusFilterPopover';
-import { RefreshPlugin } from '@/components/core/plugins/RefreshPlugin';
-import { dayjs } from '@/lib/dayjs';
-import { paths } from '@/paths';
-import Chip from '@mui/material/Chip';
-import Link from '@mui/material/Link';
-import { PencilSimple as PencilSimpleIcon } from '@phosphor-icons/react/dist/ssr/PencilSimple';
-import { getRecordList, getUsers } from './_lib/actions';
-import { defaultUser } from './_lib/types';
-import { useRouter } from 'next/navigation';
 import { CardTitle } from '@/components/cardTitle/CardTitle';
 import { PageContainer } from '@/components/container/PageContainer';
+import { dayjs } from '@/lib/dayjs';
+import { paths } from '@/paths';
+import {
+  DataGrid
+} from '@mui/x-data-grid';
+import { PencilSimple as PencilSimpleIcon } from '@phosphor-icons/react/dist/ssr/PencilSimple';
+import { useRouter } from 'next/navigation';
+import { getRecordList } from './_lib/actions';
 
-
+const useFakeMutation = () => {
+  return React.useCallback(
+    (user) =>
+      new Promise < Partial < User >> ((resolve, reject) => {
+        setTimeout(() => {
+          if (user.name?.trim() === '') {
+            reject(new Error('Error while saving user: name cannot be empty.'));
+          } else {
+            resolve({ ...user, name: user.name?.toUpperCase() });
+          }
+        }, 200);
+      }),
+    []
+  );
+};
 
 export default function Page() {
   const router = useRouter();
+  const mutateRow = useFakeMutation();
   const [users, setUsers] = React.useState([]);
   const [loading, setLoading] = React.useState(true);
   const [openModal, setOpenModal] = React.useState(false);
@@ -63,6 +72,20 @@ export default function Page() {
     setModalData(data);
   }
 
+  const processRowUpdate = React.useCallback(
+    async (newRow) => {
+      console.log(newRow, "new row ........")
+      // Make the HTTP request to save in the backend
+      const response = await mutateRow(newRow);
+      return response;
+    },
+    [mutateRow]
+  );
+
+  const handleProcessRowUpdateError = React.useCallback((error) => {
+    console.log({ children: error.message, severity: 'error' });
+  }, []);
+
 
 
   React.useEffect(() => {
@@ -70,56 +93,78 @@ export default function Page() {
   }, [pagination])
 
   const columns = [
+    { field: 'name', headerName: 'Name', width: 180, editable: true },
     {
-      formatter: (row) => (
-        <IconButton onClick={() => router.push(paths.dashboard.records + '/' + (row.id))}>
-          <PencilSimpleIcon />
-        </IconButton>
-      ),
-      name: 'Actions',
-      // hideName: true,
-      // align: 'right',
+      field: 'age',
+      headerName: 'Age',
+      type: 'number',
+      editable: true,
+      align: 'left',
+      headerAlign: 'left',
     },
     {
-      formatter: (row) => (
-        <Typography color="text.secondary" variant="body2">
-          {row.title}
-        </Typography>
-      ),
-      name: 'Title',
+      field: 'dateCreated',
+      headerName: 'Date Created',
+      type: 'text',
+      width: 180,
+      editable: true,
     },
     {
-      formatter: (row) => (
-        <Typography color="text.secondary" variant="body2">
-          {row.REVO_pinterest}
-        </Typography>
-      ),
-      name: 'Revo pinterest',
+      field: 'lastLogin',
+      headerName: 'Last Login',
+      type: 'text',
+      width: 220,
+      editable: true,
     },
     {
-      formatter: (row) => (
-        <Typography color="text.secondary" variant="body2">
-          {row.YT_account_used}
-        </Typography>
-      ),
-      name: 'YT account used',
+      field: 'custom',
+      headerName: 'custom',
+      width: 220,
+      editable: true,
     },
-    {
-      formatter: (row) => (
-        <Typography color="text.secondary" variant="body2">
-          {row.tiktok_accounts_used}
-        </Typography>
-      ),
-      name: 'Tiktok accounts used',
-    },
-    {
-      formatter(row) {
-        return dayjs(row.createdAt).format('MMM D, YYYY h:mm A');
-      },
-      name: 'Created at',
-    },
+  ];
 
-
+  const rows = [
+    {
+      id: 1,
+      name: "fazly",
+      age: 25,
+      dateCreated: "2024-14-12",
+      lastLogin: "2024-14-12",
+      custom: 'custom data',
+    },
+    {
+      id: 2,
+      name: "fazly",
+      age: 36,
+      dateCreated: "2024-14-12",
+      lastLogin: "2024-14-12",
+      custom: 'custom data',
+    },
+    {
+      id: 3,
+      name: "fazly",
+      age: 19,
+      dateCreated: "2024-14-12",
+      lastLogin: "2024-14-12",
+      custom: 'custom data',
+    },
+    {
+      id: 4,
+      name: "fazly",
+      age: 28,
+      dateCreated: "2024-14-12",
+      lastLogin: "2024-14-12",
+      custom: 'custom data',
+    },
+    {
+      id: 5,
+      name: "fazly",
+      age: 23,
+      dateCreated: "2024-14-12",
+      lastLogin: "2024-14-12",
+      custom: 'custom data',
+    },
   ];
 
 
@@ -138,49 +183,21 @@ export default function Page() {
         error={null}
       >
         <Card>
-          <Box sx={{ overflowX: 'auto' }}>
-            <React.Fragment>
-              <DataTable
-                isPagination={true}
-                totalRecords={totalRecords}
-                rowsPerPageOptions={pagination.limit}
-                pageNo={pagination.pageNo}
-                columns={columns}
-                rows={users}
-                uniqueRowId="id"
-                selectionMode="multiple"
-
-                leftItems={<>
-                  <FilterButton
-                    displayValue={status}
-                    label="Status"
-                    onFilterApply={(value) => {
-                      setStatus(value)
-                    }}
-                    onFilterDelete={() => {
-                      handlePhoneChange();
-                    }}
-                    popover={<StatusFilterPopover />}
-                    value={status}
-                  />
-                  <RefreshPlugin onClick={fetchList} />
-                </>}
-
-                rightItems={<></>}
-
-                onRowsPerPageChange={(pageNumber, rowsPerPage) => setPagination({ pageNo: pageNumber, limit: rowsPerPage })}
-                onPageChange={(newPageNumber) => setPagination({ ...pagination, pageNo: newPageNumber })}
-                onSelection={(selectedRows) => setSelectedRows?.(selectedRows)}
-              />
-              {!users?.length ? (
-                <Box sx={{ p: 3 }}>
-                  <Typography color="text.secondary" sx={{ textAlign: 'center' }} variant="body2">
-                    No customers found
-                  </Typography>
-                </Box>
-              ) : null}
-            </React.Fragment>
+          <Box sx={{ overflowX: 'auto', height: '100%', width: '100%' }}>
+            <DataGrid
+              rows={rows}
+              columns={columns}
+              processRowUpdate={processRowUpdate}
+              onProcessRowUpdateError={handleProcessRowUpdateError}
+            />
           </Box>
+          {!users?.length ? (
+            <Box sx={{ p: 3 }}>
+              <Typography color="text.secondary" sx={{ textAlign: 'center' }} variant="body2">
+                No customers found
+              </Typography>
+            </Box>
+          ) : null}
         </Card>
       </PageLoader>
     </PageContainer>
